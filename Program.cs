bfd65434146222dd4fa783c24df060281ad21eb2
Lee Berg <leealanberg@gmail.com>
2021-01-25 21:46:51 -0600

    Demo1

--- Program.cs
+++ Program.cs
@@ -19,7 +19,10 @@ namespace ShitPostSecurityBlazor
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+            
             await builder.Build().RunAsync();
         }
+
+        
     }
 }
